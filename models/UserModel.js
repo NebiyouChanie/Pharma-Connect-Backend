1ecb77eecba500d2b4d5538c6a52ea4a4e716932
NebiyouChanie <cnebiyu@gmail.com>
2025-01-18 20:12:32 +0300

    update owner role

--- models/UserModel.js
+++ models/UserModel.js
@@ -34,7 +34,7 @@ const UserSchema = new mongoose.Schema({
     },
     role :{ 
         type: String, 
-        enum: ['user', 'admin','pharmacist'], 
+        enum: ['user', 'admin','pharmacist','owner'], 
         default: 'user' 
     },
     pharmacyId: { 
